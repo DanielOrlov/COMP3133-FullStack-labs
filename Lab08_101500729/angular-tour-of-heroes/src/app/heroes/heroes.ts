ece5ab20562dc633cd3c6375ac8c6ff16ba20ed8
Daniel <daniil.orlov.ua@gmail.com>
2026-03-29 23:36:19 -0400

    exercise 2 done

--- Lab08_101500729/angular-tour-of-heroes/src/app/heroes/heroes.ts
+++ Lab08_101500729/angular-tour-of-heroes/src/app/heroes/heroes.ts
@@ -3,11 +3,12 @@ import { CommonModule, NgFor, NgIf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HEROES } from '../mock-heroes';
 import { Hero } from '../hero';
+import { RemoveSpacesPipe } from '../remove-spaces-pipe';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [CommonModule, FormsModule, NgIf, NgFor],
+  imports: [CommonModule, FormsModule, NgIf, NgFor, RemoveSpacesPipe],
   templateUrl: './heroes.html',
   styleUrls: ['./heroes.css']
 })
